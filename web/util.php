76e26ff6b6fb2fd7ec6e10330cd0fcedd6475fe7
Terry Brady <terrywbrady@gmail.com>
2014-08-15 13:23:11 -0700

    extra param check

--- web/util.php
+++ web/util.php
@@ -24,7 +24,10 @@ class util {
     }
 
 	public static function getArg($name, $def) {
-		if (isset($_GET[$name])) return $_GET[$name];
+		if (isset($_GET[$name])) {
+			if ($_GET[$name] == null) return $def;
+			return $_GET[$name];
+		}
 		return $def; 
 	}
 	
@@ -50,7 +53,10 @@ class util {
 	
 
 	public static function getPostArg($name, $def) {
-		if (isset($_POST[$name])) return $_POST[$name];
+		if (isset($_POST[$name])) {
+			if ($_POST[$name] == null) return $def;
+			return $_POST[$name];
+		}
 		return $def; 
 	}
 
