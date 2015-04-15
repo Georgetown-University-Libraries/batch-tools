75a07bac5c86662f0db7543ea38a5c154b4b41fa
Terry Brady <terrywbrady@gmail.com>
2015-04-15 16:36:42 -0700

    x

--- web/rest/restClient.js
+++ web/rest/restClient.js
@@ -13,23 +13,32 @@ $(document).ready(function(){
 		function(data){
 			$.each(data, function(index, coll){
 				var tr = addTr($("#table"));
-				tr.attr("cid", coll.id).addClass(index % 2 == 0 ? "odd" : "even");
+				tr.attr("cid", coll.id).attr("index",index).addClass(index % 2 == 0 ? "odd" : "even");
 				addTd(tr, index).addClass("num");
 				addTd(tr, "").addClass("title comm");
 				addTdAnchor(tr, coll.name, "/handle/" + coll.handle).addClass("title");
 				addTd(tr, coll.numberItems).addClass("num");
-				$.getJSON(
-					"/rest/collections/"+coll.id+"?expand=parentCommunityList",
-					function(data) {
-						var par = data.parentCommunityList[data.parentCommunityList.length-1];
-						tr.find("td.comm").append(getAnchor(par.name, /handle/" + par.handle));
-					}
-				)
+				doRow(index);
 			});
 		}
 	);
 });
 
+function doRow(row) {
+	var tr = $("tr[index="+row+"]");
+	if (tr.not("*")) return; 
+	var cid = tr.attr("cid");
+	$.getJSON(
+		"/rest/collections/"+cid+"?expand=parentCommunityList",
+		function(data) {
+			var par = data.parentCommunityList[data.parentCommunityList.length-1];
+			tr.find("td.comm").append(getAnchor(par.name, "/handle/" + par.handle));
+			doRow(row+1);
+		}
+	);
+}			
+			
+			
 function addTr(tbl) {
 	var tr = $("<tr/>");
 	tbl.append(tr);
