0ee8f5fc02ff9d6115f05e1a0a933582f38a4594
Terry Brady <terrywbrady@gmail.com>
2015-04-15 15:11:14 -0700

    x

--- web/rest/restClient.js
+++ web/rest/restClient.js
@@ -1,11 +1,42 @@
 $(document).ready(function(){
-	$("#report").append($("<div>Hello!</div>"));
+	var tbl = $("<table id='table'/>");
+	$("#report").replace(tbl);
+	var tr = addTr(tbl);
+	addTd(tr, "Num"));
+	addTd(tr, "Collection"));
+	
+	
+	$("#table").append()
 	$.getJSON(
 		"/rest/collections",
 		function(data){
 			$.each(data, function(index, coll){
-				$("#report").append($("<div>"+index+". "+coll.name+"</div>"));
+				var tr = addTr($("#table"));
+				addTd(tr, index);
+				addTdAnchor(tr, coll.name, "/handle/" + coll.handle);
 			});
 		}
 	);
 });
+
+function addTr(tbl) {
+	var tr = $("<tr/>");
+	tbl.append(tr);
+	return tr;
+}
+
+function addTd(tr, val) {
+	var td = $("<td/>");
+	if (val != null) {
+		td.append(val);
+	}
+	tr.append(td);
+	return td;
+}
+
+function getTdAnchor(tr, val, href) {
+	var a = $("<a/>");
+	a.append(val);
+	a.attr("href", href);
+	return addTd(tr, a);
+}
