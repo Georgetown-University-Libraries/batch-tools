ed9fc5a10b8703272f9e32502b00a5edef624c8c
Terry Brady <terrywbrady@gmail.com>
2015-04-16 10:28:48 -0700

    x

--- web/rest/restClient.js
+++ web/rest/restClient.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	addTd(tr, "Collection").addClass("title");
 	addTd(tr, "Num Items");
 
-	var itbl = $("<div id='itemdiv'><table/></div>");
+	var itbl = $("<div id='itemdiv'><h1>Items</h1><table/></div>");
 	itbl.attr("id","itemtable").addClass("sortable");
 	$("#report").append(itbl);
 	$("#itemdiv").dialog();
