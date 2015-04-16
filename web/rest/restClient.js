fe098dfeeef805bb983da23b65dcb2494b5587d3
Terry Brady <terrywbrady@gmail.com>
2015-04-16 10:54:43 -0700

    x

--- web/rest/restClient.js
+++ web/rest/restClient.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	addTd(tr, "Collection").addClass("title");
 	addTd(tr, "Num Items");
 
-	$("#itemdiv").dialog({title: "Items", width: "400px", modal: true}).hide();
+	$("#itemdiv").dialog({title: "Items", width: "400px", modal: true, hide: true});
 
 	$.getJSON(
 		"/rest/collections",
