89a79c1ab4cd1e62b1e6c7e6036a7c1100b0d012
Terry Brady <terrywbrady@gmail.com>
2015-04-29 12:56:50 -0700

    Create params for get request

--- web/rest/restQuery.js
+++ web/rest/restQuery.js
@@ -17,7 +17,7 @@ function loadMetadataFields() {
 }
 
 function drawShowFields() {
-	var sel = $("<select name='show-fields'/>").attr("multiple","true").attr("size","8").appendTo("#show-fields");
+	var sel = $("<select class='query-tool' name='show-fields[]'/>").attr("multiple","true").attr("size","8").appendTo("#show-fields");
 	$.each(metadataSchemas, function(index, schema){
 		$.each(schema.fields, function(findex, field) {
 			var name = field.name;
@@ -30,7 +30,7 @@ function drawShowFields() {
 
 function drawFilterQuery() {
 	var div = $("<div class='metadata'/>").appendTo("#queries");
-	var sel = $("<select name='query_field[]'/>");
+	var sel = $("<select class='query-tool' name='query_field[]'/>");
 	var opt = $("<option/>");
 	sel.append(opt);
 	$.each(metadataSchemas, function(index, schema){
@@ -42,7 +42,7 @@ function drawFilterQuery() {
 		});
 	});
 	div.append(sel);
-	sel = $("<select name='query_op[]'/>");
+	sel = $("<select class='query-tool' name='query_op[]'/>");
 	$("<option>exists</option>").val("exists").appendTo(sel);
 	$("<option>does not exist</option>").val("not_exists").appendTo(sel);
 	$("<option selected>equals</option>").val("equals").appendTo(sel);
@@ -57,7 +57,7 @@ function drawFilterQuery() {
 		$(this).parent("div.metadata").find("input[name='query_val[]']").val("").attr("readonly",disableval);
 	});
 	div.append(sel);
-	var input = $("<input name='query_val[]'/>");
+	var input = $("<input class='query-tool' name='query_val[]'/>");
 	div.append(input);
 	$("<button class='field_plus'>+</button>").appendTo(div).click(function(){
 		drawFilterQuery(metadataSchemas);
@@ -74,4 +74,24 @@ function queryButtons() {
 	$("button.field_plus:last").attr("disabled",false);
 	$("button.field_minus").attr("disabled",false);
 	$("div.metadata:first button.field_minus").attr("disabled",true);	
+	
+	$("#query-button").click(function(){runQuery();})
+}
+
+function runQuery() {
+	var params = {
+		"query_field[]" : [],
+		"query_op[]"    : [],
+		"query_val[]"   : [],
+		"show_fields[]" : [],
+		"limit"         : 100,
+		"offset"        : 0,
+	};
+	$("select.query-tool,input.query-tool").each() {
+		var paramArr = params[$(this).attr("name")];
+		paramArr[paramArr.length] = $(this).val();
+	}
+	params.limit = $("#limit").val();
+	params.offset = $("#offset").val();
+	$.getJSON("/rest/filtered-items", params);
 }
