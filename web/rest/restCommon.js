82a3fc4d6c2f9c12283f35f4051ea59f9ed0073c
Terry Brady <terrywbrady@gmail.com>
2015-05-01 14:18:55 -0700

    Refactor file containment

--- web/rest/restCommon.js
+++ web/rest/restCommon.js
@@ -2,34 +2,6 @@ var filterString = "none";
 var loadId = 0;
 var THREADS = 1;
 
-function createCollectionTable() {
-	var tbl = $("<table/>");
-	tbl.attr("id","table").addClass("sortable");
-	$("#report").replaceWith(tbl);
-
-	var tr = addTr(tbl).addClass("header");
-	addTh(tr, "Num").addClass("num").addClass("sorttable_numeric");
-	addTh(tr, "Community").addClass("title");
-	addTh(tr, "Collection").addClass("title");
-	addTh(tr, "Num Items").addClass("sorttable_numeric");
-
-
-	$.getJSON(
-		"/rest/collections?limit=500",
-		function(data){
-			$.each(data, function(index, coll){
-				var tr = addTr($("#table"));
-				tr.attr("cid", coll.id).attr("index",index).addClass(index % 2 == 0 ? "odd data" : "even data");
-				addTd(tr, index).addClass("num");
-				addTd(tr, "").addClass("title comm");
-				addTdAnchor(tr, coll.name, "/handle/" + coll.handle).addClass("title");
-				addTdAnchor(tr, coll.numberItems, "javascript:drawItemTable("+coll.id+",'')").addClass("num");
-			});
-			loadData();
-		}
-	);	
-}
-
 function createFilterTable() {
 	addFilter("","None","De-select all filters","none").click(
 		function(){
@@ -87,18 +59,6 @@ function addFilter(val, title, description, cname) {
 	return input;
 }
 
-function loadData() {
-	loadId++;
-	$("td.datacol,th.datacol").remove();
-	filterString = getFilterList();
-	doRow(0, THREADS, loadId);
-}
-
-function loadParent() {
-	doRowParent(0, THREADS);
-}
-
-
 function getFilterList() {
 	var list="";
 	$("input:checked[name='filters[]']").each(
@@ -113,100 +73,6 @@ function getFilterList() {
 		list = "none";
 	}
 	return list;
-}
-
-function doRowParent(row, threads) {
-	var tr = $("tr[index="+row+"]");
-	if (!tr.is("*")) return; 
-	var cid = tr.attr("cid");
-	$.getJSON(
-		"/rest/collections/"+cid+"?expand=parentCommunityList",
-		function(data) {
-			var par = data.parentCommunityList[data.parentCommunityList.length-1];
-			tr.find("td.comm:empty").append(getAnchor(par.name, "/handle/" + par.handle));
-			
-			sorttable.makeSortable($("#table")[0]);
-			if (row % threads == 0 || threads == 1) {
-				for(var i=1; i<=threads; i++) {
-					doRowParent(row+i, threads);
-				}					
-			}
- 		}
-	);
-}			
-
-function doRow(row, threads, curLoadId) {
-	if (loadId != curLoadId) return;
-	var tr = $("tr[index="+row+"]");
-	if (!tr.is("*")) return; 
-	var cid = tr.attr("cid");
-	$.getJSON(
-		"/rest/filtered-collections/"+cid+"?filters=" + filterString,
-		function(data) {
-			$.each(data.itemFilters, function(index, itemFilter){
-				if (loadId != curLoadId) {
-					return;
-				}
-				var trh = $("tr.header");
-				var filterName = itemFilter["filter-name"];
-				var filterTitle = itemFilter.title == null ? filterName : itemFilter.title;
-				var icount = itemFilter["item-count"];
-				if (!trh.find("th."+filterName).is("*")) {
-					var th = addTh(trh, filterTitle);
-					th.addClass(filterName).addClass("datacol").addClass("sorttable_numeric");
-					
-					if (itemFilter.description != null) {
-						th.attr("title", itemFilter.description);											
-					}
-
-					$("tr.data").each(function(){
-						var td = addTd($(this), "");
-						td.addClass(filterName).addClass("num").addClass("datacol");
-					});
-				}
-				
-				if (icount == null || icount == "0") {
-					tr.find("td."+filterName).text("0");
-				} else {
-					var anchor = getAnchor(icount,"javascript:drawItemTable("+cid+",'"+ filterName +"')");
-					tr.find("td."+filterName).append(anchor);						
-				}
-				
-				
-			});
-			
-			sorttable.makeSortable($("#table")[0]);
-			if (row % threads == 0 || threads == 1) {
-				for(var i=1; i<=threads; i++) {
-					doRow(row+i, threads, curLoadId);
-				}					
-			}
- 		}
-	);
-}			
-			
-function drawItemTable(cid, filter, collname) {
-	var itbl = $("#itemtable");
-	itbl.find("tr").remove("*");
-	var tr = addTr(itbl).addClass("header");
-	addTh(tr, "Num").addClass("num").addClass("sorttable_numeric");
-	addTh(tr, "Handle");
-	addTh(tr, "Item").addClass("title");
-	$.getJSON(
-		"/rest/filtered-collections/"+cid+"?expand=items&filters="+filter,
-		function(data){
-			var source = filter == "" ? data.items : data.itemFilters[0].items;
-			
-			$.each(source, function(index, item){
-				var tr = addTr(itbl);
-				tr.addClass(index % 2 == 0 ? "odd data" : "even data");
-				addTd(tr, index+1).addClass("num");
-				addTdAnchor(tr, item.handle, "/handle/" + item.handle);
-				addTd(tr, item.name).addClass("ititle");
-			});
-			$("#itemdiv").dialog({title: filter + " Items in " + data.name, width: "80%", minHeight: 500, modal: true});
-		}
-	);
 }
 
 function addTr(tbl) {
