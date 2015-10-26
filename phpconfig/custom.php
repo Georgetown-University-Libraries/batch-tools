b8fb42d4fb6ba3165ce45cccf99e0d29f7ec8ef5
Terry Brady <terrywbrady@gmail.com>
2015-10-26 13:53:40 -0700

    x

--- phpconfig/custom.php
+++ phpconfig/custom.php
@@ -51,7 +51,7 @@ class custom {
 	public function isCheckFilter($name) {return false;}
 	
 	public function getIngestLoc() {return "/var/dspace/ingest/";}
-	public function getRestServiceUrl() {return "http://demo.dspace.org/rest";}
+	public function getRestServiceUrl() {return "/rest";}
 	public function showBatchTools() {return $this->isUserCollectionOwner();}
 	
 	public function isPdo() {return false;}
