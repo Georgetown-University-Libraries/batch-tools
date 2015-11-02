dc6e137de6d295cdec2da54d6b3cbb9622213db3
Terry Brady <terrywbrady@gmail.com>
2015-11-02 09:48:26 -0800

    x

--- phpconfig/customRest.php
+++ phpconfig/customRest.php
@@ -44,11 +44,9 @@ class RestInitializer {
 	
 	public function initCommunities() {
 		$json_a = util::json_get(custom::instance()->getRestServiceUrl() . "/communities/?expand=parentCommunity");
-		echo "foo";
-		print_r($json_a);
-		echo "foo";
-		exit;
 		foreach($json_a as $k=>$comm) {
+			print_r($k);
+			print_r($comm);
 			$pid = (isset($comm["parentCommunity"])) ? $this->getId($comm["parentCommunity"]) : null;
 			$this->initJsonCommunity($pid, $comm);
 		}
