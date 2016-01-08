fdc8b83b168428bbf7e4c57df256d64c872a7a3c
Terry Brady <terrywbrady@gmail.com>
2016-01-08 15:46:28 -0800

    increase timeout redirect for kie uplod

--- web/qcReport.js
+++ web/qcReport.js
@@ -363,7 +363,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
   function jobQueue() {
 	  $('#filterSubmit,#ingestSubmit,#changeParentSubmit,#reindexSubmit').attr('disabled',true);
 	  $("#submitting").show();
-	  setTimeout(function(){document.location="../web/queue.php"}, 5000)
+	  setTimeout(function(){document.location="../web/queue.php"}, 8000)
   }
   
   function jobQueueUpload() {
