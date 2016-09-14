dd39c029e15c1b95ce32f80695bb9cd3452e473b
Terry Brady <terrywbrady@gmail.com>
2016-09-14 15:28:24 -0700

    handle case of a comm with no collections

--- web/stats/solrStats.php
+++ web/stats/solrStats.php
@@ -28,7 +28,7 @@ if ($comm != "") {
             $q .= "id:" . $col;
         }
         $q .= ")";
-    } else {
+    } else if ($colls != ""){
         $q="(";
         foreach(explode(",",$colls) as $col) {
             if ($q != "(") {
