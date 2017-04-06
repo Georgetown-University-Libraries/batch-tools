6b7295fd0e00d86605b726b50fd097f344935b98
Terry W Brady <twb27@georgetown.edu>
2017-04-06 14:48:26 -0700

    x

--- web/stats/qcItemStats.php
+++ web/stats/qcItemStats.php
@@ -4,11 +4,14 @@ include '../header.php';
 ini_set('max_execution_time', 120);
 
 $CUSTOM = custom::instance();
+$shards = $CUSTOM->getSolrShards();
+$qroot = $CUSTOM->getSolrPath() . "statistics/select?shards={$shards}&rows=0&wt=json";
 
 $item=util::getArg("item","");
 $ititle = $item == "" ? "All Items" : $item;
-$isearch = "type:2" . $item == "" ? "" : "+AND+id:{$item}";
-$bsearch = "type:0+AND+bundle:ORIGINAL" . $item == "" ? "" : "+AND+owningItem:{$item}";
+$isearch = "&q=type:2" . $item == "" ? "" : "+AND+id:{$item}";
+$bsearch = "&q=type:0+AND+bundle:ORIGINAL" . $item == "" ? "" : "+AND+owningItem:{$item}";
+$facet = "&facet=true&facet.date=time&facet.date.start=NOW/MONTH/DAY-30MONTHS&facet.date.end=NOW&facet.date.gap=".urlencode("+1MONTH");
 
 header('Content-type: text/html; charset=UTF-8');
 
@@ -16,12 +19,16 @@ header('Content-type: text/html; charset=UTF-8');
 <html>
 <head>
 <?php 
-$header = new LitHeader();
+$header = new LitHeader($item);
 $header->litPageHeader();
 ?>
 
 <script type="text/javascript">
-
+  $(document).ready(function(){
+    $.getJSON("{$qroot}{$facet}{$isearch}", function(data){
+      $("#foo").text(data);
+    });
+  });
 </script>
 <style type="text/css">
 </style>
@@ -37,6 +44,7 @@ $header->litPageHeader();
 
 
 <div id="ins">
+<div id="foo"/>
 <table class="sortable">
 <tbody>
 <tr  class='header'>
