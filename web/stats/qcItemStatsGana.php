244391d5210db085136404e2974815bf62640345
Terry W Brady <twb27@georgetown.edu>
2017-04-07 16:41:28 -0700

    x

--- web/stats/qcItemStatsGana.php
+++ web/stats/qcItemStatsGana.php
@@ -7,8 +7,9 @@ header('Content-type: application/json');
 
 $CUSTOM = custom::instance();
 $handle=trim(util::getArg("handle",""));
-$key = $CUSTOM->getExtConfig()["GANAKEY"];
-$id  = $CUSTOM->getExtConfig()["GANAID"];
+$arr = $CUSTOM->getExtConfig();
+$key = $arr["GANAKEY"];
+$id  = $arr["GANAID"];
 
 $url = "https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A{$id}&start-date=3000daysAgo&end-date=yesterday&metrics=ga%3Apageviews&dimensions=ga%3Ayear%2Cga%3Amonth&filters=ga%3ApagePath%3D%3D%2Fhandle%2F{$handle}&key={$key}";
 echo file_get_contents($url);
