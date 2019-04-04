1ea2877970df58af2f20189aaf743b281fe041e9
Terry Brady <terrywbrady@gmail.com>
2019-04-04 11:08:04 -0700

    fix accession count

--- web/accessionByDate.php
+++ web/accessionByDate.php
@@ -9,13 +9,13 @@ Copyright (c) 2013, Georgetown University Libraries All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
-in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials 
-provided with the distribution. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 include 'header.php';
@@ -27,7 +27,7 @@ header('Content-type: text/html; charset=UTF-8');
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<?php 
+<?php
 $header = new LitHeader("Item Count By Accession Date");
 $header->litPageHeader();
 ?>
@@ -48,7 +48,7 @@ $(function(){
 </script>
 </head>
 <body>
-<?php 
+<?php
 $header->litHeader(array());
 $sql = <<< HERE
 select
@@ -59,7 +59,7 @@ where metadata_field_id = (
   select metadata_field_id
   from metadatafieldregistry
   where element='date' and qualifier='accessioned'
-) and place=1
+)
 group by month
 union
 select
@@ -70,7 +70,7 @@ select
     select metadata_field_id
     from metadatafieldregistry
     where element='date' and qualifier='accessioned'
-  ) and place=1
+  )
   group by month
 union
 select
@@ -81,7 +81,7 @@ where metadata_field_id = (
   select metadata_field_id
   from metadatafieldregistry
   where element='date' and qualifier='accessioned'
-) and place=1
+) 
 group by month
 order by month;
 HERE;
